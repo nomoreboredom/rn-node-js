3a5ddb288605fe5c6509b7fa5cfd58a35cc51a9b
Arda Ilgaz <ardailgaz@hotmail.com>
2019-06-08 16:47:22 +0200

    rn nodeify added and configured

--- android/app/src/main/java/com/nodejsexampleapp/MainApplication.java
+++ android/app/src/main/java/com/nodejsexampleapp/MainApplication.java
@@ -3,6 +3,10 @@ package com.nodejsexampleapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.tradle.react.UdpSocketsModule;
+import com.peel.react.TcpSocketsModule;
+import com.bitgo.randombytes.RandomBytesPackage;
+import com.peel.react.rnos.RNOSModule;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +26,11 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new UdpSocketsModule(),
+            new TcpSocketsModule(),
+            new RandomBytesPackage(),
+            new RNOSModule()
       );
     }
 
