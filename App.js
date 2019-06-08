e5e8209c99d546cbc2098b6342ee17a005c3edb6
Arda Ilgaz <ardailgaz@hotmail.com>
2019-06-08 16:47:33 +0200

    crypto example

--- App.js
+++ App.js
@@ -7,23 +7,38 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import crypto from 'crypto'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const algorithm = 'aes-256-cbc';
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
 
 type Props = {};
+
 export default class App extends Component<Props> {
+  encrypt = (text) => {
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+  }
+  decrypt = (text) => {
+   let iv = Buffer.from(text.iv, 'hex');
+   let encryptedText = Buffer.from(text.encryptedData, 'hex');
+   let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+   let decrypted = decipher.update(encryptedText);
+   decrypted = Buffer.concat([decrypted, decipher.final()]);
+   return decrypted.toString();
+  }
   render() {
+    const res = this.encrypt("MY SECRET AND IMPORTANT THING")
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={styles.container, {margin:20}}>
+        <Text style={styles.title}>encryptedData</Text>
+        <Text style={styles.info}>{JSON.stringify(res)}</Text>
+        <Text style={styles.title}>decryptedData</Text>
+        <Text style={styles.info}>{this.decrypt(res)}</Text>
       </View>
     );
   }
@@ -36,12 +51,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  title: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
+  info: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
